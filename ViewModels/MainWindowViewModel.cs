6ab97d2f2a16bffc61c1dcd7e22d0b98e17b6a60
H97-Git <arno.demarchi.8@gmail.com>
2024-05-22 16:48:45 +0200

    First Commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.ObjectModel;
+using ReactiveUI;
+
+namespace Sandbox.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    private ObservableCollection<string> _observableCollection =
+    [
+        "Data", "Data", "Data", "Data",
+        "Data", "Data", "Data", "Data",
+        "Data", "Data", "Data", "Data",
+        "Data", "Data", "Data", "Data",
+        "Data", "Data", "Data", "Data",
+        "Data", "Data", "Data", "Data",
+        "Data", "Data", "Data", "Data",
+        "Data", "Data", "Data", "Data",
+        "Data", "Data", "Data", "Data",
+        "Data", "Data", "Data", "Data",
+        "Data", "Data", "Data", "Data",
+        "Data", "Data", "Data", "Data",
+        "Data", "Data", "Data", "Data",
+        "Data", "Data", "Data", "Data"
+    ];
+
+    public ObservableCollection<string> ObservableCollection
+    {
+        get => _observableCollection;
+        set => this.RaiseAndSetIfChanged(ref _observableCollection, value);
+    }
+}
